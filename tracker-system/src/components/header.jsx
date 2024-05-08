63bd36f9c12f732e8d8b7735bc9f377c1b0c0a57
Anjee <94264810+Th0mas00@users.noreply.github.com>
2024-05-08 23:13:07 +0800

    tailwind css initialize

---
+++ tracker-system/src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function header() {
+    return (
+        <div>
+            <h1>header</h1>
+        </div>
+    )
+}
---
+++ tracker-system/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function Login() {
+    return (
+        <div>
+            <h1 className="text-3xl text-green-600 font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
